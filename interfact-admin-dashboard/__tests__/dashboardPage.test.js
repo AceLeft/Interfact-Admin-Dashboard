b760cd697c6eebe9be1d1f01425b207273dfd92c
Brooke Wright <brookerwright2@gmail.com>
2024-12-01 16:17:41 -0500

    Mock useRouter in Page test

--- interfact-admin-dashboard/__tests__/dashboardPage.test.js
+++ interfact-admin-dashboard/__tests__/dashboardPage.test.js
@@ -3,10 +3,22 @@ import { render, screen } from '@testing-library/react'
 import Home from '../src/app/dashboard/page.tsx'
 import React from 'react'
 
+// Mock useRouter:
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    jest.fn(() => ({
+      push: jest.fn(),
+      replace: jest.fn(),
+    }))
+  }
+}));
  
 describe('Home', () => {
   it('has no created intersections', () => {
-    render(<Home />)
+
+    render(
+         <Home />
+  )
  
     const intersectionList = screen.getByText("showing 0 intersections", {exact: false})
  
