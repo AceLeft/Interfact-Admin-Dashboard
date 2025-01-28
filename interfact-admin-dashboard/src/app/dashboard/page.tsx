6b4b3c077b9b42fd3add82b5d8fe44ac6ca908d8
An-Unavailable-Username <111703932+An-Unavailable-Username@users.noreply.github.com>
2025-01-28 13:03:11 -0500

    Reformatted code, added line by line comments to better understand logic flow / make easier to read.

--- interfact-admin-dashboard/src/app/dashboard/page.tsx
+++ interfact-admin-dashboard/src/app/dashboard/page.tsx
@@ -13,63 +13,123 @@ import calculateDifferenceInMinutes from "./timeCaculator";
 
 export default function Dashboard() {
 
+    //------------------------------ Hooks -----------------------------
 
+    // Hooks to get data or utilities
     const intersections = useIntersections();
     const userFeedback = useUserFeedback();
     const router = useRouter();
 
+    // React Hooks handle the state of the const
     const [ isFiltering, setIsFiltering ] = useState<boolean | null>(null);
     const [ isFilterOpen, setIsFilterOpen ] = useState<boolean | null>(false);
     const [ isFilterBlocked, setIsFilterBlocked ] = useState<boolean | null>(false);
-
     const [ intersectionsShown, setintersectionsShown] = useState<number | null>(0);
-
     const [refreshKey, setRefreshKey] = useState<number>(0);
 
+    //------------------------------------------------------------------
+
+
+
+
+    //---------------------------- User Report & Intersection Elements -------------------------------
+
+    // Total # of intersections
+    const totalIntersections = intersections.length;
+
+    // Calculates total # of reports from user feedback 
     const getTotalReports = userFeedback.reduce((total, user) => {
         return total + (user.reports ? user.reports.length : 0);
     }, 0);
 
-    const totalIntersections = intersections.length;
+    // Gets # of reports for an intersection id
+    const getReportsForIntersection = (intersectionId: string) => {
+        return userFeedback.reduce((total, user) => {
+            if (user.reports) {
+                const matchingReports = user.reports.filter((report) => report.reportid === intersectionId);
+                return total + matchingReports.length;
+                }
+            return total;
+            }, 0);
+        };
 
+    //------------------------------------------------------------------------------------------------
+
+
+
+
+    //----------------------------------- Page Utility Elements --------------------------------------
+
+    // Refreashes the page 
     const refreshPage = () => {
+        // Refreash page key
         setRefreshKey((prevKey) => prevKey + 1); 
+        // Reset the filters
         setIsFiltering(null)
         setIsFilterOpen(false)
         setIsFilterBlocked(false)
     };
 
-    const getReportsForIntersection = (intersectionId: string) => {
-        return userFeedback.reduce((total, user) => {
-            if (user.reports) {
-            const matchingReports = user.reports.filter(
-                (report) => report.reportid === intersectionId
-            );
-            return total + matchingReports.length;
-            }
-            return total;
-        }, 0);
-    };
-
+    // Navigate to intersection page by the intersection id
     const navToIntersectionPage = (id: string) =>{
         router.push(`/Intersection/${id}`)
     }
 
+    // Redirect component to Interfact.live
+    const interfactLiveRedirect = () => {
+        window.open('https://interfact.live/map', '_blank');
+      };
+
+    const goToAddCamera = () =>{
+        router.push("/add_camera")
+    }
+
+    // Keyboard shortcuts
+    useEffect(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            // If the c key is pressed
+            if (event.key.toLowerCase() === 'c') {
+                // Redirect to add camera page
+                router.push('/add_camera');
+            }
+        };
+        // EventListener is needed for keydown events
+        document.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [router]);
+
+    //------------------------------------------------------------------------------------------------
+
+
+
+
+    //----------------------------------- Filter Elements --------------------------------------------
+
+    // Toggles the state of the filters
     const filterIntersections = () =>{
+        // Toggles true or false
         setIsFiltering(!isFiltering) 
     } 
 
+    // Handles filter options
     const filterOptions = (selectedOption: string) => {
         switch (selectedOption) {
             
             case 'Open':
+                // If blocked filter is not active
                 if(isFilterBlocked !== true){
+                    // Toggle filter to open
                     setIsFilterOpen(!isFilterOpen)
                 }
                 break;
 
             case 'Blocked':
+                // If open filter is not active
                 if(isFilterOpen !== true){
+                    // Toggles filter to blocked
                     setIsFilterBlocked(!isFilterBlocked)
                 }
                 break;
@@ -80,41 +140,26 @@ export default function Dashboard() {
         }
     };
 
+    // Filters intersections based on applied filters
     const filteredIntersections = intersections.filter((item) => {
+        // Shows open intersections only if the filter is applied
         if (isFilterOpen) return item.status === 'OPEN';
+
+        // Shows only blocked intersections if the filter is applied
         if (isFilterBlocked) return item.status === 'BLOCKED';
         //return true; // No filters applied
     });
 
+    // Updates # of intersections shown when filteredIntersections changes
+    // setIntersectionsShown hook will run whenever filteredIntersections changes
     useEffect(() => {
         setintersectionsShown(filteredIntersections.length);
     }, [filteredIntersections]);
     
+    //------------------------------------------------------------------------------------------------
 
-    const interfactLiveRedirect = () => {
-        window.open('https://interfact.live/map', '_blank');
-      };
 
-    const goToAddCamera = () =>{
-        router.push("/add_camera")
-    }
-    //------------------------------------------------------------------------------
-    useEffect(() => {
-        const handleKeyPress = (event: KeyboardEvent) => {
-            if (event.key.toLowerCase() === 'c') {
-                // Redirect to add camera page
-                router.push('/add_camera');
-            }
-        };
-        document.addEventListener('keydown', handleKeyPress);
-
-        return () => {
-            document.removeEventListener('keydown', handleKeyPress);
-        };
-    }, [router]);
-    //------------------------------------------------------------------------------
-
-      
+    // Dashboard UI 
     return(
         <div>
             <div className="top-dashboard shadow">
@@ -124,14 +169,22 @@ export default function Dashboard() {
                     <div className="dash-main-2">Total Intersections | <span>{totalIntersections}</span></div>
                     <div className="dash-main-3">Problems Reported (Last 30 days) | <span>{getTotalReports}</span></div>
                 </div>
+
+                {/* Open map view BUTTON */}
                 <button onClick={interfactLiveRedirect} className='map-view'>Map View</button>
+                {/* Refresh page BUTTON */}
                 <button onClick={refreshPage} className='refresh-button'><FontAwesomeIcon icon={faArrowsRotate}/></button>
             </div>
+
             <div className="filter">
+                    {/* Toggle filter bar BUTTON */}
                     <button onClick={filterIntersections} className='filter-1 shadow'><FontAwesomeIcon icon={faFilter}/> Filter</button>
+                    
+                    {/* Shows # of filtered intersections */}
                     <div className='filter-2'>showing {intersectionsShown} intersections</div>
             </div>
 
+            {/* Toggles filterbar shadow based on if isFiltering is filtering */}
             <div className={`filter-bar shadow ${isFiltering ? '': 'hidden'}`}>
                 <div className='blocked-open'>
 
