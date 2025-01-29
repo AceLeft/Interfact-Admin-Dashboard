cea509447a499982d9542adff6ea23b109b541ef
Brooke Wright <brookerwright2@gmail.com>
2025-01-29 16:47:32 -0500

    Add failing tests for intersection amounts displayed on Cities

---
+++ interfact-admin-dashboard/__tests__/citiesPage.test.js
@@ -0,0 +1,46 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, within } from '@testing-library/react';
+import React from 'react';
+import Cities from '../src/app/cities/page.tsx'
+import { useIntersections } from '../src/app/hooks/useIntersections';
+import { describe } from 'node:test';
+
+// Mock useRouter:
+const mockPush = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: mockPush,
+    replace: jest.fn(),
+  }),
+}));
+
+// Mock useIntersections
+jest.mock('../src/app/hooks/useIntersections', () => ({
+  useIntersections: jest.fn(),
+}));
+
+
+
+describe('Muncie tab', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('show correct number of intersections', () => {
+    // Value doesnt matter, just how many
+    const intersectionsData = [{},{},{}];
+
+    useIntersections.mockReturnValue(intersectionsData);
+
+    render(<Cities />);
+
+    expect(screen.getByText('Intersections | ' + intersectionsData.length)).toBeInTheDocument();
+  });
+
+  it('shows 0 intersections if none', () => {
+    useIntersections.mockReturnValue([]);
+    render(<Cities />);
+    expect(screen.getByText('Intersections | 0')).toBeInTheDocument();
+  });
+});
--- interfact-admin-dashboard/src/app/cities/page.tsx
+++ interfact-admin-dashboard/src/app/cities/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-export default function cities() {
+export default function Cities() {
 
     const router = useRouter();
 
