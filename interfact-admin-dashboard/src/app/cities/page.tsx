8fcd154b5cae3109f82894a54e041d45b8af6815
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-02-02 21:18:25 -0500

    Fixed Maintenance bug plus city count

--- interfact-admin-dashboard/src/app/cities/page.tsx
+++ interfact-admin-dashboard/src/app/cities/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { useRouter } from 'next/navigation';
-
+import { useIntersections } from "../hooks/useIntersections";
 export default function Cities() {
 
     const router = useRouter();
+    const intersectionCount = useIntersections()
 
     const toCityDashboard = () =>{
         router.push("/dashboard")
@@ -17,7 +18,7 @@ export default function Cities() {
                     <div className="city-item shadow">
                         <div className="city-item-text1">Muncie</div>
                         <hr />
-                        <div className="city-item-text2">Intersections | 4</div>   
+                        <div className="city-item-text2">Intersections | {intersectionCount.length}</div>   
                     </div>
                 </div>
             </div>
