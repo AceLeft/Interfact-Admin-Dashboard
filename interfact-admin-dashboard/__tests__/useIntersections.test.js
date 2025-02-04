5db03dffc3863a10232792ca43183eb1e5cdaa9e
An-Unavailable-Username <111703932+An-Unavailable-Username@users.noreply.github.com>
2025-02-04 12:07:43 -0500

    Added the tests that were missing from the newly updated main

--- interfact-admin-dashboard/__tests__/useIntersections.test.js
+++ interfact-admin-dashboard/__tests__/useIntersections.test.js
@@ -1,7 +1,15 @@
 
 import { renderHook } from "@testing-library/react"
 import {useIntersections} from "../src/app/hooks/useIntersections.ts"
+import { mockGoogleCloudFirestore } from "firestore-jest-mock";
 
+mockGoogleCloudFirestore({
+  database: {
+    intersections: [
+      {id:'1', name: 'test'}
+    ]
+  }
+})
 
 describe("useIntersections", () => {
   it("should return the default input", async () => {
